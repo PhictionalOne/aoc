c609d182976b1aecd5ab6fa340deb82b9d2c18ce
PhictionalOne <code@phictional.de>
2021-12-04 16:35:38 +0100

    Solve AOC 2021 day 04

---
+++ 2021/04/aoc2021-day04.hs
@@ -0,0 +1,211 @@
+{- 
+# Day 4: Giant Squid
+
+You're already almost 1.5km (almost a mile) below the surface of the ocean,
+already so deep that you can't see any sunlight. What you **can** see, however,
+is a giant squid that has attached itself to the outside of your submarine.
+
+Maybe it wants to play bingo?
+
+Bingo is played on a set of boards each consisting of a 5x5 grid of numbers.
+Numbers are chosen at random, and the chosen number is **marked** on all boards
+on which it appears. (Numbers may not appear on all boards.) If all numbers in
+any row or any column of a board are marked, that board **wins**. (Diagonals
+don't count.)
+
+The submarine has a **bingo subsystem** to help passengers (currently, you and
+the giant squid) pass the time. It automatically generates a random order in
+which to draw numbers and a random set of boards (your puzzle input). 
+For example:
+
+```
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+```
+
+After the first five numbers are drawn (`7`, `4`, `9`, `5`, and `11`), there
+are no winners, but the boards are marked as follows (shown here adjacent to
+each other to save space):
+
+```
+22  13  17  11*  0      3  15   0   2  22     14  21  17  24   4*
+ 8   2  23   4* 24      9* 18  13  17   5*    10  16  15   9* 19
+21   9* 14  16   7*    19   8   7* 25  23     18   8  23  26  20
+ 6  10   3  18   5*    20  11* 10  24   4*    22  11* 13   6   5*
+ 1  12  20  15  19     14  21  16  12   6      2   0  12   3   7*
+```
+
+After the next six numbers are drawn (`17`, `23`, `2`, `0`, `14`, and `21`),
+there are still no winners:
+
+```
+22  13  17* 11*  0*     3  15   0*  2* 22     14* 21* 17* 24   4*
+ 8   2* 23*  4* 24      9* 18  13  17*  5*    10  16  15   9* 19
+21*  9* 14* 16   7*    19   8   7* 25  23*    18   8  23* 26  20
+ 6  10   3  18   5*    20  11* 10  24   4*    22  11* 13   6   5*
+ 1  12  20  15  19     14* 21* 16  12   6      2*  0* 12   3   7*
+```
+
+Finally, `24` is drawn:
+
+```
+22  13  17* 11*  0*     3  15   0*  2* 22     14* 21* 17* 24*  4*
+ 8   2* 23*  4* 24*     9* 18  13  17*  5*    10  16  15   9* 19
+21*  9* 14* 16   7*    19   8   7* 25  23*    18   8  23* 26  20
+ 6  10   3  18   5*    20  11* 10  24*  4*    22  11* 13   6   5*
+ 1  12  20  15  19     14* 21* 16  12   6      2*  0* 12   3   7*
+```
+
+At this point, the third board **wins** because it has at least one complete
+row or column of marked numbers (in this case, the entire top row is marked:
+`14` `21` `17` `24` `4`).
+
+The **score** of the winning board can now be calculated. Start by finding the
+**sum of all unmarked numbers** on that board; in this case, the sum is 188.
+Then, multiply that sum by **the number that was just called** when the board
+won, `24`, to get the final score, `188 * 24 = `**`4512`**.
+
+To guarantee victory against the giant squid, figure out which board will win
+first. **What will your final score be if you choose that board?**
+-}
+import Data.List
+
+toInt :: String -> Int
+toInt = (foldl (\xs x -> xs * 10 + x) 0) . (map (\x -> (fromEnum x -48)))
+
+replace :: Eq a => a -> a -> [a] -> [a]
+replace f t = map (\x -> if x == f then t else x)
+
+parseCSV :: String -> [Int]
+parseCSV = (map toInt) . words . (replace ',' ' ')
+
+type Board = [[(Int, Bool)]]
+
+parseBoards :: [String] -> [Board]
+parseBoards input = buildBoards input
+  where buildBoards :: [String] -> [Board]
+        buildBoards [] = []
+        buildBoards bs = (buildField . takeB) bs : buildBoards (dropB bs)
+                                     -- Number, Marker
+        buildField = map ((map (\x -> (toInt x, False))) . words)
+
+        takeB :: [String] -> [String]
+        takeB = (takeWhile ((0 <) . length)) . tail
+        dropB :: [String] -> [String]
+        dropB = (dropWhile ((0 <) . length)) . tail
+        
+
+-- Examples
+sampleBoardString = "1,2,3,3,42,12,4\n\n1 34 23 2\n4 3 23 9\n98 45 3 2\n34 2 34 42\n\n12 23 34 56\n56 67 78 89\n90 10 20 30\n91 82 73 64"
+sampleBoards = (parseBoards . tail . lines) sampleBoardString
+
+-- sampleBoard == head sampleBoardList
+sampleBoard :: Board
+sampleBoard = [ [( 1, False), (34, False), (23, False), ( 2, False)]
+              , [( 4, False), ( 3, False), (23, False), ( 9, False)]
+              , [(98, False), (45, False), ( 3, False), ( 2, False)]
+              , [(34, False), ( 2, False), (34, False), (42, False)] ]
+
+
+sampleBingo :: Board
+sampleBingo = [ [( 1, False), (34, False), (23, True ), ( 2, False)]
+              , [( 4, True ), ( 3, True ), (23, True ), ( 9, True )]
+              , [(98, False), (45, False), ( 3, True ), ( 2, False)]
+              , [(34, False), ( 2, False), (34, True ), (42, False)] ]
+
+
+isBingo :: Board -> Bool
+isBingo b = isHorizontal b || isVertical b
+  where isHorizontal = (foldr (||) False) . (map (foldr ((&&) . snd) True))
+        isVertical   = (isHorizontal . transpose) 
+
+mark :: Int -> Board -> Board
+mark m b = map (replace (m, False) (m, True)) b
+
+play :: [Board] -> [Int] -> ([Board], Int)
+play bs xs = foldl markAndSweep (bs, head xs) xs
+  where markAndSweep :: ([Board], Int) -> Int -> ([Board], Int)
+        markAndSweep (bs, prev) x = if ((0 <) . length) bingos 
+                                    then (bingos, prev)
+                                    else (map (mark x) bs, x)
+          where bingos = filter isBingo bs
+
+
+partOne :: String -> Int
+partOne input = sumOfUnmarked wins * lastCall
+  where puzzle  = lines input
+        numbers = (parseCSV . head) puzzle
+        boards  = (parseBoards . tail) puzzle
+
+        (wins, lastCall) = play boards numbers
+        sumOfUnmarked = sum . (map fst) . (filter (not . snd)) . (foldl (++) []) . head
+
+{-
+## Part Two
+On the other hand, it might be wise to try a different strategy: let the giant
+squid win.
+
+You aren't sure how many bingo boards a giant squid could play at once, so
+rather than waste time counting its arms, the safe thing to do is to **figure
+out which board will win last** and choose that one. That way, no matter which
+boards it picks, it will win for sure.
+
+In the above example, the second board is the last to win, which happens after
+`13` is eventually called and its middle column is completely marked. If you
+were to keep playing until this point, the second board would have a sum of
+unmarked numbers equal to `148` for a final score of `148 * 13 = `**`1924`**.
+
+Figure out which board will win last. **Once it wins, what would its final
+score be?**
+-}
+
+play2 :: [Board] -> [Int] -> ([Board], Int, (Board, Int))
+play2 bs xs = foldl markAndSweep (bs, head xs, ([[(0, True)]],0)) xs
+  where markAndSweep :: ([Board], Int, (Board, Int)) -> Int -> ([Board], Int, (Board, Int))
+        markAndSweep (bs, prev, lb) x = (map (mark x) nobingos, x, lastBingo)
+          where nobingos = filter (not . isBingo) bs
+                lastBingo = let search = filter isBingo bs
+                            in if ((0 <) . length) search
+                               then (head search, prev)
+                               else lb
+
+
+partTwo :: String -> Int
+partTwo input = sumOfUnmarked win * lastCall
+  where puzzle  = lines input
+        numbers = (parseCSV . head) puzzle
+        boards  = (parseBoards . tail) puzzle
+
+        (_, _, (win, lastCall)) = play2 boards numbers
+        sumOfUnmarked = sum . (map fst) . (filter (not . snd)) . (foldl (++) [])
+
+
+{- 
+### Main
+-}
+main :: IO ()
+main = do
+  puzzle <- readFile "puzzle.aoc"
+  print "What will your final score be if you choose that board?"
+  print $ partOne puzzle
+  print "Once it wins, what would its final score be?"
+  print $ partTwo puzzle
+
+
--- 2021/README.md
+++ 2021/README.md
@@ -4,6 +4,7 @@
 - [Day 1](01/aoc2021-day01.md)
 - [Day 2](02/aoc2021-day02.md)
 - [Day 3](03/aoc2021-day03.md)
+- [Day 4](04/aoc2021-day04.md)
 
 ## Requirements
 - A Haskell installation (GHC)
